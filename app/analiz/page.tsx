7594cca0eeaa3875fb3ebdaf39d100164b1e1c0a
Mary <ytu.iremkurt@gmail.com>
2025-07-05 01:57:54 +0300

    Fix: Move 'use client' to top of analiz page to resolve Next.js build error

--- app/analiz/page.tsx
+++ app/analiz/page.tsx
@@ -1,6 +1,5 @@
+"use client";
 export const dynamic = "force-dynamic";
-
-"use client"
 
 import { useEffect, useState } from "react"
 import { useSearchParams, useRouter, usePathname } from "next/navigation"
